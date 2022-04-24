b35a724098ab51848cbd15426cf03913e56e7531
KiryshaYama <79928594+KiryshaYama@users.noreply.github.com>
2022-04-24 20:19:35 +0500

    fix def predict_salary
    rewrite function

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ from terminaltables import SingleTable
 
 
 def predict_salary(salary_from, salary_to):
-    if not salary_from and not salary_to:
-        return None
-    if not salary_from:
-        return 0.8 * salary_to
-    if not salary_to:
-        return 1.2 * salary_from
     if salary_from and salary_to:
         return (salary_from + salary_to)/2
+    if salary_from:
+        return 1.2 * salary_from
+    if salary_to:
+        return 0.8 * salary_to
 
 
 def parse_hh(language):
